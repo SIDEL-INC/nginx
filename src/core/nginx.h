4b23a5dc42867291a5dc016dfddfd120c6da3b49
Kangjun Heo <kheo@sidelcorp.com>
2023-09-02 23:39:35 +0900

    remove version info

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -9,9 +9,9 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1025002
-#define NGINX_VERSION      "1.25.2"
-#define NGINX_VER          "nginx/" NGINX_VERSION
+#define nginx_version      0
+#define NGINX_VERSION      ""
+#define NGINX_VER          "" NGINX_VERSION
 
 #ifdef NGX_BUILD
 #define NGINX_VER_BUILD    NGINX_VER " (" NGX_BUILD ")"
