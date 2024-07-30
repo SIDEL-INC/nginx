72053e20c8a2cca5120a2a0f295eb5ebc3ea1f70
Kangjun Heo <knowledge@o.cnu.ac.kr>
2024-07-30 12:26:01 +0900

    more

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 make clean
-./configure --with-http_ssl_module --with-http_v2_module --with-http_v3_module --without-mail_pop3_module --without-mail_imap_module --without-mail_smtp_module --without-http_fastcgi_module --without-http_uwsgi_module --without-http_scgi_module --prefix=/opt/nginx --sbin-path=/sbin/nginx --http-client-body-temp-path=/tmp/body_temp --http-log-path=/logs
+./configure --with-http_ssl_module --with-http_v2_module --with-http_v3_module --without-mail_pop3_module --without-mail_imap_module --without-mail_smtp_module --without-http_fastcgi_module --without-http_uwsgi_module --without-http_scgi_module --prefix=/opt/nginx --sbin-path=/sbin/nginx --http-client-body-temp-path=/tmp/body_temp --http-log-path=/logs --http-proxy-temp-path=/tmp/proxy_temp
 make -j $(nproc)
