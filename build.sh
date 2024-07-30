6e336f69525f0f0dca0bb41f16ec6b30eac9bd27
Kangjun Heo <knowledge@o.cnu.ac.kr>
2024-07-30 12:03:33 +0900

    make with appropriate amount of processors

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 make clean
 ./configure --with-http_ssl_module --with-http_v2_module --with-http_v3_module --without-mail_pop3_module --without-mail_imap_module --without-mail_smtp_module --without-http_fastcgi_module --without-http_uwsgi_module --without-http_scgi_module --prefix=/opt/nginx
-make -j24
+make -j $(nproc)
