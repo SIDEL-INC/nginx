f039ffa5e5cfd1e80a481078688fbcc83f3c94d4
Kangjun Heo <kheo@sidelcorp.com>
2025-02-18 18:21:08 +0900

    no ssl modules issue

--- build.sh
+++ build.sh
@@ -4,17 +4,17 @@
 
 #install dependencies
 case $ID in
-	debian|ubuntu) sudo apt-get install -y libpcre3 libpcre3-dev
+	debian|ubuntu) sudo apt-get install -y libpcre3 libpcre3-dev libssl-dev
   ;;
 
-	redhat|rocky|ol) sudo dnf install -y pcre pcre-devel
+	redhat|rocky|ol) sudo dnf install -y pcre pcre-devel openssl-devel
   ;;
 
 	*) echo "Unknown distribution; skipping"
   ;;
 esac
 
-MODULES=--with-http_ssl_module --with-http_v2_module --with-http_v3_module --without-mail_pop3_module --without-mail_imap_module --without-mail_smtp_module --without-http_fastcgi_module --without-http_uwsgi_module --without-http_scgi_module
+MODULES="--with-http_ssl_module --with-http_v2_module --with-http_v3_module --without-mail_pop3_module --without-mail_imap_module --without-mail_smtp_module --without-http_fastcgi_module --without-http_uwsgi_module --without-http_scgi_module"
 PREFIX=/opt/webserver
 SBIN=/sbin/webserver
 LOGPATH=/var/log/webserver
@@ -22,5 +22,5 @@ TMPPATH=/tmp
 PIDPATH=/run/webserver
 
 make clean
-./configure --prefix=$PREFIX --sbin-path=$SBIN --http-client-body-temp-path=$TMPPATH/body_temp --http-log-path=$LOGPATH/server.log --error-log-path=$LOGPATH/error.log --http-proxy-temp-path=$TMPPATH/proxy_temp --pid-path=$PIDPATH/.pid --lock-path=$PIDPATH/.lock
+./configure $MODULES --prefix=$PREFIX --sbin-path=$SBIN --http-client-body-temp-path=$TMPPATH/body_temp --http-log-path=$LOGPATH/server.log --error-log-path=$LOGPATH/error.log --http-proxy-temp-path=$TMPPATH/proxy_temp --pid-path=$PIDPATH/.pid --lock-path=$PIDPATH/.lock
 make -j $(nproc)
