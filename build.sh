d667a824dbb5327448e358bb48ed395425cb0209
Kangjun Heo <kheo@sidelcorp.com>
2025-11-23 15:56:16 +0900

    fix build.sh

--- build.sh
+++ build.sh
@@ -15,11 +15,11 @@ case $ID in
 esac
 
 MODULES="--with-http_realip_module --with-http_ssl_module --with-http_v2_module --with-http_v3_module --without-mail_pop3_module --without-mail_imap_module --without-mail_smtp_module --without-http_fastcgi_module --without-http_uwsgi_module --without-http_scgi_module"
-PREFIX=/opt/webserver
-SBIN=/sbin/ws
-LOGPATH=/opt/webserver/logs
+PREFIX=/opt/nginx
+SBIN=/sbin/nginx
+LOGPATH=/logs
 TMPPATH=/tmp
-PIDPATH=/run/webserver
+PIDPATH=/run/ws
 
 make clean
 ./configure $MODULES --prefix=$PREFIX --sbin-path=$SBIN --http-client-body-temp-path=$TMPPATH/body_temp --http-log-path=$LOGPATH/server.log --error-log-path=$LOGPATH/error.log --http-proxy-temp-path=$TMPPATH/proxy_temp --pid-path=$PIDPATH/.pid --lock-path=$PIDPATH/.lock
